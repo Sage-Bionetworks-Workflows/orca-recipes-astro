ce1a2a486ec6b990e6b66dfc21d5af69245a8f2f
Brad Macdonald <bwm03570@gmail.com>
2022-11-15 13:54:27 -0700

    cleans up

--- dags/nf-hello.py
+++ dags/nf-hello.py
@@ -16,20 +16,29 @@ from sagetasks.nextflowtower.utils import TowerUtils
     tags=["hello"])
 
 def nf_hello_dag():
-    """
-    1. Opens Tower workspace
-    2. Launches hello world workflow
-    """
 
     @task(multiple_outputs=True)
     def open_tower_workspace():
+        """
+        Opens tower workspace - things are hard coded for the moment that would be parameterized in future versions
+
+        Returns:
+            dict: TowerUtils class instance within dictionary for easy variable passing
+        """
         tower_token = os.environ["TOWER_ACCESS_TOKEN"]
         client_args = TowerUtils.bundle_client_args(tower_token, platform="sage", debug_mode=False)
         tower_utils = TowerUtils(client_args)
         return {'tower_utils': tower_utils}
 
     @task()
-    def launch_tower_workflow(tower_utils, workspace_id):
+    def launch_tower_workflow(tower_utils: sagetasks.nextflowtower.utils.TowerUtils, workspace_id: str):
+        """
+        Launches tower workflow
+
+        Args:
+            tower_utils (sagetasks.nextflowtower.utils.TowerUtils): TowerUtils class instance
+            workspace_id (str): Workspace ID for tower run
+        """
         tower_utils.open_workspace(workspace_id)
         tower_utils.launch_workflow(
             compute_env_id="1QX5bol8rZHBZkTAEIvQts",
